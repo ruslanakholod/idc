0a2588995ef5e05e169dcb81f8264e5c25431b59
Ruslana <39275648+ruslanakholod@users.noreply.github.com>
2018-06-01 15:55:26 +0300

    create html and scss files (#3)
    * create html and scss files
    
    * create form
    
    * changed the form
    
    * fixed errors
    
    * changed fix-form
    
    * changed fix-form

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,36 @@
-console.log("test gulp");
+$( document ).ready(function() {
+  //mask phone
+  jQuery(function($) {
+    $("#phone").mask("+375 (99) 999-99-99");
+  });
+  //timer
+  function updater(day, hour, minute, second) {
+    const baseTime = new Date(2018, 5, 28);
+    let period = 8*24*60*60*1000;
+    function update() {
+      let current = new Date();
+      let difference = period - (current - baseTime) % period;
+      let millis = difference % 1000;
+      difference = Math.floor(difference / 1000);
+      let sec = difference % 60;
+      if (sec < 10)
+      sec = "0" + sec;
+      difference = Math.floor(difference / 60);
+      let min = difference % 60;
+      if (min < 10)
+      min = "0" + min;
+      difference = Math.floor(difference / 60);
+      let hours = difference % 24;
+      if (hours < 10)
+      hours = "0" + hours;
+      let days = Math.floor(difference / 24);
+      day.innerHTML = days;
+      hour.innerHTML = hours;
+      minute.innerHTML = min;
+      setTimeout(update, millis);
+    }
+    setTimeout(update, 0);
+  }
+  updater(document.getElementById("days"),
+   document.getElementById("hours"), document.getElementById("minutes"));
+});
